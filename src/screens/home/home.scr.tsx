d3d4bc1428307186df0d9428044dd700a77a33c5
Lucía Seggiaro <luciaseggiaro@gmail.com>
2022-08-05 00:11:57 -0300

    v.2.0.1: FontAwesome added, HeaderLay now has a navbar

--- src/screens/home/home.scr.tsx
+++ src/screens/home/home.scr.tsx
@@ -4,7 +4,7 @@ import "./home.scss";
 
 export const HomeScr: any = () => {
 return(
-  <section className="Home">
+  <section className="Home" id="home">
   <img
     src={siteInConstruction}
     className="Home-img"
@@ -14,14 +14,14 @@ return(
     Hi there! My portfolio is currently under construction.
   </p>
   <p className="Home-paragraph paragraph-regular-regular">
-    To find out more about my work click{" "}
+    To find out more about my work check{" "}
     <a
       className="Home-paragraph-link"
       href="https://www.linkedin.com/in/lucia-seggiaro/"
       target="_blank"
       rel="noopener noreferrer"
     >
-      here
+      my LinkedIn profile
     </a>
     .
   </p>
--- src/shared/layouts/header/header.lay.tsx
+++ src/shared/layouts/header/header.lay.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 import "./header.scss";
 
 export const HeaderLay: any = () => {
-return(
-  <header className="Header">
-  </header>
-)
-}
+  const [navbarShown, setNavbarShown] = useState<Boolean>(true);
+  const currentYear: number = new Date().getFullYear();
+  return (
+    <header className="Header paragraph-regular-regular ">
+      <nav className="Header-nav">
+        <button
+          className="Header-nav-button"
+          onClick={() => {
+            setNavbarShown(!navbarShown);
+          }}
+        >
+          <FontAwesomeIcon
+            icon={navbarShown ? faXmark : faBars}
+            className="Header-nav-button-icon"
+          />
+        </button>
+        {navbarShown && (
+          <ul
+            className={`Header-nav-list ${
+              navbarShown ? "Header-nav-list--isShown" : ""
+            }`}
+          >
+            <li className="Header-nav-list-item">
+              <a href="#home" className="Header-nav-list-item-link">
+                Home
+              </a>
+            </li>
+            <li className="Header-nav-list-item">
+              <a href="#" className="Header-nav-list-item-link">
+                About
+              </a>
+            </li>
+            <li className="Header-nav-list-item">
+              <a href="#" className="Header-nav-list-item-link">
+                Services
+              </a>
+            </li>
+            <li className="Header-nav-list-item">
+              <a href="#" className="Header-nav-list-item-link">
+                Experience
+              </a>
+            </li>
+            <li className="Header-nav-list-item">
+              <a href="#" className="Header-nav-list-item-link">
+                Works
+              </a>
+            </li>
+            <li className="Header-nav-list-item">
+              <a href="#" className="Header-nav-list-item-link">
+                Blog
+              </a>
+            </li>
+            <li className="Header-nav-list-item">
+              <a href="#" className="Header-nav-list-item-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        )}
+      </nav>
+      <p className="Header-copyright">&#169; Lucía Seggiaro - {currentYear}</p>
+    </header>
+  );
+};
